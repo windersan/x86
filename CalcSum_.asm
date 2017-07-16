4c00065f87401028ff552efb66e6196bb85c4e8f
Devin Andres Salemi <devinsalem@gmail.com>
2017-07-16 00:42:10 -0700

    Add files via upload

--- CalcSum_.asm
+++ CalcSum_.asm
@@ -1,14 +1,18 @@
     .model flat,c
     .code
 
+
 CalcSum_ proc
 
         push ebp
         mov ebp,esp
-        mov eax,[ebp+8]                     
+        mov eax,[ebp+8]                   
+        mov ecx,[ebp+12]                    
         mov edx,[ebp+16]                   
         add eax,ecx                        
-        add eax,edx                         
+        add eax,edx      
+		mov ecx,[ebp+20]  
+		add eax, ecx
         pop ebp
         ret
 
