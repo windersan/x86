c7d3068e67f2ee5273e35fad2a019b7a43f3f678
Devin Andres Salemi <devinsalem@gmail.com>
2017-07-16 00:43:00 -0700

    Add files via upload

--- CalcSum.cpp
+++ CalcSum.cpp
@@ -1,11 +1,11 @@
 #include "stdafx.h"
 
-extern "C" int CalcSum_(int a, int b, int c);
+extern "C" int CalcSum_(int a, int b, int c, int d);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    int a = 40000, b = 10000, c = 20000;
-    int sum = CalcSum_(a, b, c);
+	int a = 40000, b = 10000, c = 20000, d = 1;
+    int sum = CalcSum_(a, b, c, d);
     printf("  x1:   %d\n", a);
     printf("  x2:   %d\n", b);
     printf("  x3:   %d\n", c);
